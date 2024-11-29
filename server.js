fbc9dbc5d74e5434e35b6371508474e91aa8d09c
xAL6 <xcultivation@gmail.com>
2024-11-29 17:59:51 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const authRoute = require("./routes").auth;
+const courseRoute = require("./routes").course;
+const passport = require("passport");
+require("./config/passport")(passport);
+const cors = require("cors");
+const  path=require("path");
+const port = process.env.PORT || 8080; // process.env.PORT是Heroku自動動態設定
+
+
+// 連結MongoDB
+mongoose
+  .connect(MONGODB_CONNECTION)
+  .then(() => {
+    console.log("連結到mongodb...");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.static(path.join(__dirname,'client','build')));
+
+app.use("/api/user", authRoute);
+// course route應該被jwt保護
+// 如果request header內部沒有jwt，則request就會被視為是unauthorized
+app.use(
+  "/api/courses",
+  passport.authenticate("jwt", { session: false }),
+  courseRoute
+);
+
+if(process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging") {
+  app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','build','index.html'));
+  })
+}
+
+app.listen(port, () => {
+  console.log("後端伺服器聆聽在port 8080...");
+});
